37f7153009a072c7660df64bc48adcaa762ac89d
gsteev <gsteev@github.com>
2017-01-30 16:34:52 -0500

    Added "Good bye" message after Hello World

--- HelloWorld/src/Hello.java
+++ HelloWorld/src/Hello.java
@@ -5,5 +5,6 @@ public class Hello {
 		// TODO Auto-generated method stub
 		System.out.println("Here ye Here ye : important announcement");
 		System.out.println("Hello World");
+		System.out.println("Good bye");
 	}
 }
