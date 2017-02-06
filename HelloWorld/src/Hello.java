8c6a78e1b587cc6ff6d2d5c2fd6d95df97b8914d
gsteev <gsteev@github.com>
2017-02-06 13:36:54 -0500

    Second user merging to Repository

--- HelloWorld/src/Hello.java
+++ HelloWorld/src/Hello.java
@@ -6,5 +6,7 @@ public class Hello {
 		System.out.println("Here ye Here ye : important announcement");
 		System.out.println("Hello World");
 		System.out.println("Good bye");
+		
+		System.out.println("This should cause merge conflict");
 	}
 }
